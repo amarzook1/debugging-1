f936a0648968d47f072005fa39b84ff7bb7df60e
Ahmed <ahmed.marzook@cgi.com>
2018-10-04 11:22:06 +0000

    Debugged decoding issue

--- debugging.rb
+++ debugging.rb
@@ -14,20 +14,12 @@ def decode(ciphertext, key)
   end
   plaintext_chars.join
 end
-
-# Intended output:
-#
-# > encode("theswiftfoxjumpedoverthelazydog", "secretkey")
-# => "EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL"
-#
-# > decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
-# => "theswiftfoxjumpedoverthelazydog"
 =end
 
 def encode(plaintext, key)
   cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
   ciphertext_chars = plaintext.chars.map do |char|
-    (65 + cipher.find_index(char).to_i).chr
+    (65 + cipher.find_index(char).to_i).chr #add a to_i due to an error when converting
   end
   ciphertext_chars.join
 end
@@ -35,7 +27,7 @@ end
 def decode(ciphertext, key)
   cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
   plaintext_chars = ciphertext.chars.map do |char|
-    cipher[65 - char.ord]
+    cipher[char.ord - 65] #The problem was the char.ord - 65 was the other way round giving a totally different output
   end
   plaintext_chars.join
 end
